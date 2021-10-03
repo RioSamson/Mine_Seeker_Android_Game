cf436c43216f4b8cfae356f0dc0e13a5198e3121
Rio Samson <rbs5@sfu.ca>
2021-10-02 18:09:30 -0700

    Add method to gameManager to retrieve games at certain index. Implemented List View for main screen to show games. @Rio Samson

--- app/src/main/java/ca/cmpt276/as2/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as2/MainActivity.java
@@ -15,17 +15,23 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import ca.cmpt276.as2.databinding.ActivityMainBinding;
+import ca.cmpt276.as2.model.Game;
 import ca.cmpt276.as2.model.GameManager;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
-    private GameManager gameManager = new GameManager();
+    private GameManager gameManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
 
+        gameManager = GameManager.getInstance();
+        populateListView();
+        setupFloatingActionButton();
+        setupListViewClick();
+
+    }
+
+    private void setupListViewClick() {
+        ListView list = (ListView) findViewById(R.id.listViewGames);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                TextView textView = (TextView) viewClicked;
+                String message = "Clicked #" + position + " - " + textView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void populateListView() {
+        //turn list of games into a string array
+
+        String[] myGames = {"rio", "is", "great"};
+        //DONT DELETE  - NEED ONCE IMPLEMENTED USERINPUT AND CAN FILL GAME MANAGER!!!!!!
+        /*
+        String[] myGames = new String[gameManager.length()];
+        int i = 0;
+        for (Game game : gameManager) {
+            myGames[i] = game.toString();
+        }
+    */
+
+        //builds the adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.items_for_listview,
+                myGames);
+
+        ListView list = (ListView) findViewById(R.id.listViewGames);
+        list.setAdapter(adapter);
+    }
+
+    private void setupFloatingActionButton() {
         binding.fab.setOnClickListener(view -> {
             Intent i = SaveGame.makeLaunchIntent(MainActivity.this, "Enter Values");
             startActivity(i);
--- app/src/main/java/ca/cmpt276/as2/model/GameManager.java
+++ app/src/main/java/ca/cmpt276/as2/model/GameManager.java
@@ -11,12 +11,32 @@ import java.util.List;
 public class GameManager implements Iterable<Game>{
     private final List<Game> games = new ArrayList<>();
 
+    /*
+        Singleton Support
+     */
+    private static GameManager instance;
+    private GameManager() {
+    }
+    public static GameManager getInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    /*
+        Normal Object Code
+     */
     public void add(Game game) {
         games.add(game);
     }
 
     public void delete(int index) {
         games.remove(index);
+    }
+
+    public Game getGame(int index) {
+        return games.get(index);
     }
 
     public boolean isEmpty() {
