9d69a12fff4f5b788d3c63548d78f80f29a40288
Rio Samson <rbs5@sfu.ca>
2021-10-03 01:22:36 -0700

    Edit saveGame layout with all the needed elements. Added "up" back button with its function in saveGame activity. Added SAVE button in saveGame activity without its functionality. @Rio Samson

--- app/src/main/java/ca/cmpt276/as2/SaveGame.java
+++ app/src/main/java/ca/cmpt276/as2/SaveGame.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.navigation.NavController;
@@ -19,14 +23,7 @@ import androidx.navigation.ui.NavigationUI;
 import ca.cmpt276.as2.databinding.ActivitySaveGameBinding;
 
 public class SaveGame extends AppCompatActivity {
-
     private static final String EXTRA_MESSAGE = "Extra - message";
-
-    public static Intent makeLaunchIntent(Context c, String message) {
-        Intent intent = new Intent(c, SaveGame.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        return intent;
-    }
 
     private AppBarConfiguration appBarConfiguration;
     private ActivitySaveGameBinding binding;
@@ -37,13 +34,39 @@ public class SaveGame extends AppCompatActivity {
 
         binding = ActivitySaveGameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
 
-        //Handle the extra
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //Handle the extra message when launched to add a game
         Intent i = getIntent();
         String message = i.getStringExtra(EXTRA_MESSAGE);
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 
+        //taking in the information from user input
+//        EditText userInP1Cards  = (EditText) findViewById(R.id.p1Cards);
+//        EditText userInP1Points  = (EditText) findViewById(R.id.p1Points);
+//        EditText userInP1Wagers  = (EditText) findViewById(R.id.p1Wagers);
+//        String one = userInP1Cards.getText().toString();
+//        int p1Cards = Integer.parseInt(one);
+
+        TextView printScore = (TextView) findViewById(R.id.outScore1);
+        printScore.setText("" + 30);
+
+    }
+
+    //giving intent that include a string info
+    public static Intent makeLaunchIntent(Context c, String message) {
+        Intent intent = new Intent(c, SaveGame.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        return intent;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate menu
+        getMenuInflater().inflate(R.menu.menu_save_game, menu);
+        return true;
     }
 }
